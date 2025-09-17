95b1ebdb6f3f4511f1c60b3c5700546651061205
Fedorova_Darya <fdu12345@mail.ru>
2025-09-17 03:14:16 +0300

    Создал основу программы

---
+++ program.py
@@ -0,0 +1,12 @@
+print("Моя программа")
+name = input("Введите имя: ")
+print(f"Здравствуйте, {name}")
+
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+print(f"{a} + {b} = {a + b}")
+
+if a > b:
+    print(f"{a} > {b}")
+else:
+    print(f"{a} !> {b}")
